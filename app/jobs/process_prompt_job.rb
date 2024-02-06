15bda5c2be43e61d496217afac8e8f1ef6bcab40
Christian Bieniak <cbieniak@gmail.com>
2024-02-06 21:44:10 +1100

    Include processing text

--- app/jobs/process_prompt_job.rb
+++ app/jobs/process_prompt_job.rb
@@ -4,7 +4,8 @@ class ProcessPromptJob < ApplicationJob
   queue_as :default
 
   def perform(prompt_entry)
-    puts prompt_entry
+    prompt_entry.update(response: "processing...")
+    prompt_entry.broadcast_replace_to "prompt_updates", partial: "prompt_entries/prompt_entry", locals: { prompt_entry: prompt_entry }
 
     stdout, status = Open3.capture2("python3 phixtral/generate.py --model mlabonne/phixtral-4x2_8 --max-tokens 100 --prompt \"#{prompt_entry.prompt}\"")
     prompt_entry.update(response: stdout)
